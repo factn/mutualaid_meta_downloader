285927b603c7e2b884af397e89ba5241f4ecd2a9
Miles Thompson <utunga@gmail.com>
2020-04-26 14:43:53 +1200

    start of parsing of reach4help data

---
+++ download_reach4help_data.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var markers = require("./reach4help_data/markers");
+// update reach4help_data/markers.ts  from
+// "https://raw.githubusercontent.com/reach4help/reach4help/master/map/src/data/markers.ts");
+
+console.log(markers.MARKERS);
---
+++ download_reach4help_data.ts
@@ -0,0 +1,7 @@
+
+
+import { MarkerInfo, MARKERS } from './reach4help_data/markers';
+
+// var markers = url.parse("https://raw.githubusercontent.com/reach4help/reach4help/master/map/src/data/markers.ts");
+
+console.log(MARKERS);

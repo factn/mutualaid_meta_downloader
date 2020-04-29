d0afed6188a6a69408c9a8396df9bc616aa09004
Miles Thompson <utunga@gmail.com>
2020-04-29 22:31:26 +1200

    Add a clean and merge script

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
+# Meta Data Downloader
 
+Quick and dirty scripts to merge the common-fields of Mutual Aid like listings from a number of sources into one big list. 
 
-## Usage
+This is a ResilienceApp internal tool, for analysis and review purposes only. It is *not* intended to be a public replacement for the awesome work done at the following great sites, which are the true sources of this data and which continue to hold all relevant copyrights - not to mention much more detailed and up to date data. Go to the true sources for this information:
+
+* http://reach4help.org 
+* http://mutualaidhub.org 
+* http://mutualaid.wiki
+* https://github.com/townhallproject/mutual-aid-networks (related to mutualaidhub)
+
+## Installation
 
 ```
 npm i
-node download_mutual_aid_hub.js > mutual_aid_hub.csv
 ```
+
+## Usage
+
+1. Update any manual edits 
+
+Update `manual_classifications.csv` from AirTable
+
+2.  Download data
+
+```
+node download_mutual_aid_hub.js > mutualaid_hub.csv
+node download_mutualaidwiki.js >  mutualaid_wiki.csv
+node parse_na_mutual_aid_networks_datasource.js > na_mutual_aid_networks.csv
+node parse_reach4help_markers.js > reach4help.csv
+```
+
+3. Run merge 
+```
+node dedupe_and_merge.js > all_data.csv 
+```
+
+                                      
+
+## Data
+
+node download_recovers_org.js > 
---
+++ dedupe_and_merge.js
@@ -0,0 +1,168 @@
+'use strict';
+const _ = require('underscore')
+const csv = require('csvtojson')
+const { Parser } = require('json2csv');
+const compareUrls = require('compare-urls');
+const normalizeUrl = require('normalize-url');
+
+function merge(row, other) {
+
+    var result = {  
+            "Name": row.Name 
+                    ? row.Name 
+                    : other.Name,
+            "Classification": row.Classification 
+                    ? row.Classification 
+                    : other.Classification,
+            "Homepage": row.Homepage 
+                    ? row.Homepage 
+                    : other.Homepage,
+            "SocialMedia": row.SocialMedia 
+                    ? row.SocialMedia 
+                    : other.SocialMedia,
+            "Locality": row.Locality 
+                    ? row.Locality 
+                    : other.Locality,
+            "Lat": row.Lat 
+                    ? row.Lat 
+                    : other.Lat,
+            "Lng": row.Lng 
+                    ? row.Lng 
+                    : other.Lng,
+            "Address": row.Address 
+                    ? row.Address 
+                    : other.Address,
+            "Country": row.Country 
+                    ? row.Country 
+                    : other.Country,
+            "Description": row.Description 
+                    ? row.Description 
+                    : other.Description,
+            "SupportRequest": row.SupportRequest 
+                    ? row.SupportRequest 
+                    : other.SupportRequest,
+            "SupportOffer": row.SupportOffer 
+                    ? row.SupportOffer 
+                    : other.SupportOffer,
+            "Hotline": row.Hotline 
+                    ? row.Hotline 
+                    : other.Hotline,
+            "Tags": row.Tags 
+                    ? row.Tags 
+                    : other.Tags,
+            "Notes": row.Notes 
+                    ? row.Notes 
+                    : other.Notes,
+            "ListingSources": 
+                    row.ListingSources 
+                    + "," + other.ListingSource,
+            "ListingSourceIds": 
+                    row.ListingSourcesId 
+                    + "," + other.ListingSourceId,
+        }
+        // console.log("Merging");
+        // console.log(row);
+        // console.log("With");
+        // console.log(other);
+        // console.log("Result");
+        // console.log(result);
+        return result;
+    }
+
+
+function forceSchema(row) {
+    try {
+        var homePageUrl = normalizeUrl(row.Homepage);
+    } catch (e) {
+        var homePageUrl = "";
+    }   
+    return {  
+        "Name": row.Name,
+        "Classification": row.Classification,
+        "Homepage": homePageUrl,
+        "SocialMedia": row.SocialMedia,
+        "Locality": row.Locality,
+        "Lat": row.Lat,
+        "Lng": row.Lng,
+        "Address": row.Address,
+        "Country": row.Country,
+        "Description": row.Description,
+        "SupportRequest": row.SupportRequest,
+        "SupportOffer": row.SupportOffer,
+        "Hotline": row.Hotline,
+        "Tags": row.Tags,
+        "Notes": row.Notes,
+        "ListingSources": row.ListingSource,
+        "ListingSourcesId": row.ListingSourceId
+    }
+}
+
+// finds index os row in base that is a duplicate of the row
+// passed in, in the sense of being meaningfully the same entity 
+function matchingRowIdx(base, row) {
+
+    var idx = _.findIndex(base, (baseRow) => {
+        //return baseRow.Name === row.Name;
+        return baseRow.Name.toLowerCase() === row.Name.toLowerCase()
+    });
+
+    if (idx !== -1) {
+        try {
+            var rowUrl = normalizeUrl(row.Homepage);
+            if (compareUrls(base[idx].Homepage, rowUrl))
+                return idx;
+        } catch (e) {
+            if (base[idx].Homepage === "")
+                return idx;
+        }
+    }
+    return -1;
+}
+
+
+function mergeIn(base, other) {
+    _.each(other, (row)=> {
+        var idx = matchingRowIdx(base, row);
+        if (idx !== -1) {
+            base[idx] = merge(base[idx], row);
+        }
+        else {
+            base.push(forceSchema(row));
+        }
+
+    });
+    return base;
+}
+
+(async () => {
+
+    const reach4help = await csv()
+        .fromFile("reach4help.csv");
+    const mutualaid_hub = await csv()
+        .fromFile("mutualaid_hub.csv");
+    const mutualaid_wiki = await csv()
+        .fromFile("mutualaid_wiki.csv");
+    const na_mutual_aid_networks = await csv()
+        .fromFile("na_mutual_aid_networks.csv");
+
+
+    // merge in each in turn, 
+    // keeping data from both rows if one is blank, but 
+    // but preferring the one on the left
+    // removing dupes as it goes (sometimes there are 
+    // dupes even within one datasource)
+    var base = mergeIn([], reach4help);
+    base = mergeIn(base, mutualaid_hub);
+    base = mergeIn(base, mutualaid_wiki);
+    base = mergeIn(base, na_mutual_aid_networks);
+
+    //pipe stdout to a csv file
+    try {
+      const parser = new Parser();
+      const csv = parser.parse(base);
+      console.log(csv);
+    } catch (err) {
+      console.error(err);
+    }
+
+})();
